6e88fbe2c5f5106ffb749eb6cac1e4937e4670da
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-10 14:10:47 +0100

    affichage de la page "mes compétences" - ajout de la vue UserSkillsListView - ajout du template my-skills.html - ajout url 'my-skills/'

--- echange_de_competences/skills/urls.py
+++ echange_de_competences/skills/urls.py
@@ -5,6 +5,6 @@ from .views import skill_views, help_request_views
 app_name = "skills"
 urlpatterns = [
     path("all-skills/", skill_views.AllSkillsView.as_view(), name="all-skills"),
-    #path("my-skills/", skill_views.UserSkillsListView.as_view(), name="my-skills"),
+    path("my-skills/", skill_views.UserSkillsListView.as_view(), name="my-skills"),
     path('auth/', include('django.contrib.auth.urls')),
 ]
--- echange_de_competences/skills/views/skill_views.py
+++ echange_de_competences/skills/views/skill_views.py
@@ -1,11 +1,27 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 
-from skills.models import Skill
+from skills.models import Skill, UserSkill
 
 
 class AllSkillsView(generic.ListView):
+    """
+    Vue permettant d'afficher toutes les compétences du site
+    """
     template_name = "skill_list.html"
     context_object_name = "skill_list"
 
     def get_queryset(self):
         return Skill.objects.order_by("skill_name")
+
+
+class UserSkillsListView(LoginRequiredMixin, generic.ListView):
+    """
+    Vue permettant d'afficher les compétences de l'utilisateur connecté
+    """
+    template_name = "my-skills.html"
+    context_object_name = "my_skills_list"
+
+    def get_queryset(self):
+        # on récupère les compétences de l'utilisateur connecté
+        return UserSkill.objects.filter(user=self.request.user).select_related("skill")
