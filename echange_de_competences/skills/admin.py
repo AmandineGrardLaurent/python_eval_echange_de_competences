7ff880aa228e3b3295e5eeb5b0c293e1419044e6
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-10 11:49:35 +0100

    ajout des ModelAdmin pour l'admin Django - SkillAdmin - UserSkillAdmin - HelpRequestAdmin

--- echange_de_competences/skills/admin.py
+++ echange_de_competences/skills/admin.py
@@ -1,3 +1,22 @@
+from dataclasses import fields
+
 from django.contrib import admin
 
-# Register your models here.
+from skills.models import Skill, UserSkill, HelpRequest
+
+
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ("id", "skill_name")
+
+
+class UserSkillAdmin(admin.ModelAdmin):
+    list_display = ("id", "user__username", "skill__skill_name")
+
+
+class HelpRequestAdmin(admin.ModelAdmin):
+    list_display = ("id", "description", "skill__skill_name", "requester__username", "helper__username", "date")
+
+
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(UserSkill, UserSkillAdmin)
+admin.site.register(HelpRequest, HelpRequestAdmin)
