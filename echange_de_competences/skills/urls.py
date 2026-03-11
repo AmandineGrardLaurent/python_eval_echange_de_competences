526193f374c300d7226aeeb13218de9821a5f415
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-11 10:09:47 +0100

    mise en place de l'url "my-skills/add/" pour accéder au formulaire d'ajout des compétences

--- echange_de_competences/skills/urls.py
+++ echange_de_competences/skills/urls.py
@@ -5,7 +5,8 @@ from .views import skill_views, help_request_views
 app_name = "skills"
 urlpatterns = [
     path("all-skills/", skill_views.AllSkillsView.as_view(), name="all-skills"),
-    path("my-skills/", skill_views.UserSkillsListView.as_view(), name="my-skills"),
+    path("my-skills/", skill_views.UserSkillListView.as_view(), name="my-skills"),
+    path("my-skills/add/", skill_views.UserSkillCreateView.as_view(), name="add-my-skills"),
     path('auth/', include('django.contrib.auth.urls')),
     path("help-requests-planned/", help_request_views.AllHelpRequestPlannedView.as_view(), name="help-requests-planned"),
     path("matching-skills-help-requests/", help_request_views.AllHelpRequestBySkillView.as_view(), name="matching-skills-help-requests")
