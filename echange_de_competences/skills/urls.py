3c7cd34263e3429331aac47982c219c869b8bf18
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2026-03-11 10:46:13 +0100

    refactor(urls): simplification et hiérarchisation des routes
    - Organisation des routes personnelles sous 'my/' et 'my/add/'.
    - Uniformisation des routes d'aide sous 'help/requests/'.
    - Amélioration de la lisibilité et de la maintenance des chemins.
    - ajout d'un descriptif pour chaque route

--- echange_de_competences/skills/urls.py
+++ echange_de_competences/skills/urls.py
@@ -4,10 +4,25 @@ from .views import skill_views, help_request_views
 
 app_name = "skills"
 urlpatterns = [
-    path("all-skills/", skill_views.AllSkillsView.as_view(), name="all-skills"),
-    path("my-skills/", skill_views.UserSkillListView.as_view(), name="my-skills"),
-    path("my-skills/add/", skill_views.UserSkillCreateView.as_view(), name="add-my-skills"),
+    # URL pour accéder à toutes les compétences
+    path("all/", skill_views.AllSkillsView.as_view(), name="all-skills"),
+
+    # URL pour accéder aux compétences de l'utilisateur connecté
+    path("my/", skill_views.UserSkillListView.as_view(), name="my-skills"),
+
+    # URL pour ajouter/retirer des compétences à l'utilisateur connecté
+    path("my/add/", skill_views.UserSkillCreateView.as_view(), name="add-my-skills"),
+
+    # URL pour l'authentification
     path('auth/', include('django.contrib.auth.urls')),
-    path("help-requests-planned/", help_request_views.AllHelpRequestPlannedView.as_view(), name="help-requests-planned"),
-    path("matching-skills-help-requests/", help_request_views.AllHelpRequestBySkillView.as_view(), name="matching-skills-help-requests")
+
+    # URL pour accéder aux demandes d'aide planifiées
+    path("help/requests/planned/",
+         help_request_views.AllHelpRequestPlannedView.as_view(),
+         name="help-requests-planned"),
+
+    # URL pour accéder aux demandes en lien avec les compétences de l'utilisateur connecté
+    path("help/requests/matching-skills/",
+         help_request_views.AllHelpRequestBySkillView.as_view(),
+         name="help-requests-matching-skills")
 ]
